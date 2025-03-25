b6b57b8fa2847c473662bd22330f1afe69dcc86a
abdifatah <abdifatah4@msn.com>
2025-03-25 11:10:12 +0100

    getCartoon & getOneCartoonById

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,44 @@
 /** Import des librairies */
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
-// A schema is a collection of type definitions (hence "typeDefs")
-const cartoons = [
-  {
-    id: 1,
-    name: "Les Mystérieuses Cités d'Or",
-    description:
-      "Esteban, un jeune garçon orphelin, part à la recherche des légendaires Cités d'Or en Amérique du Sud accompagné de Zia et Tao.",
-  },
-  {
-    id: 2,
-    name: "Ulysse 31",
-    description:
-      "Ulysse se perd dans l'espace avec son équipage et cherche à rentrer sur Terre tout en affrontant les dieux de l'Olympe.",
-  },
-  {
-    id: 3,
-    name: "Dragon Ball SUPER COOL",
-    description:
-      "Son Goku, un jeune garçon doté d'une force incroyable, part à la recherche des Dragon Balls, des boules de cristal magiques.",
-  },
-];
+import { getCartoons, getOneCartoonById } from "./resolvers/cartoon.resolver";
 
 const typeDefs = `#graphql
   # This "Cartoon" type defines the queryable fields for every cartoon in our data source.
+  type Personnage{
+      id: Int
+      name: String
+      role: String
+      short_description: String
+    }
+
   type Cartoon {
     id: ID
     name: String
     description: String
+    nb_of_episodes: Int
+    nb_of_seasons: Int
+    genres: [String]
+    realisator: String
+    author: String
+    ft_diffusion: String
+    personnages: [Personnage]
   }
 
   # The "Query" type is special: it lists all of the available queries
   type Query {
-    getCartoons: [Cartoon]
+    getCartoons: [Cartoon],
+    getOneCartoonById(id: ID!): Cartoon
   }
-`// This resolver retrieves books from the "books" array above.
+`;
+
 const resolvers = {
   Query: {
-    getCartoons: () => cartoons,
+    getCartoons,
+    getOneCartoonById,
   },
 };
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
---
+++ src/resolvers/cartoon.resolver.ts
@@ -0,0 +1,11 @@
+import { default as cartoons } from "../../dataset.json";
+import { Cartoon } from "../types/cartoon.type";
+type GetOneCartoonByIdArgs = {
+  id: string;
+}
+export const getCartoons = ():Cartoon[] => {
+  return cartoons;
+} 
+export const getOneCartoonById = (_:unknown, args:GetOneCartoonByIdArgs): Cartoon => {
+  return cartoons.find((cartoon) => cartoon.id === +args.id) as Cartoon;
+}
