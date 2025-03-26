5f68425efa8bb1a92deb145fa5004e0e38bd068c
abdifatah <abdifatah4@msn.com>
2025-03-26 16:12:37 +0100

    create cartoon mutation

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,25 @@
 /** Import des librairies */
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
-import { getCartoons, getOneCartoonById } from "./resolvers/cartoon.resolver";
+import {
+  createCartoon,
+  getCartoons,
+  getOneCartoonById,
+} from "./resolvers/cartoon.resolver";
 import { Personnage, PersonnageInput } from "./schemas/personnage.schema";
-import { Cartoon } from "./schemas/cartoon.schema";
+import { Cartoon, CartoonInput } from "./schemas/cartoon.schema";
 
 const typeDefs = `#graphql
   # This "Cartoon" type defines the queryable fields for every cartoon in our data source.
   type Cartoon ${Cartoon}
   type Personnage ${Personnage}
+
   input PersonnageInput ${PersonnageInput}
+  input CartoonInput ${CartoonInput}
+
+  type Mutation {
+    createCartoon(cartoon: CartoonInput!): ID,
+  }
   # The "Query" type is special: it lists all of the available queries
   type Query {
     getCartoons: [Cartoon],
@@ -21,6 +31,9 @@ const resolvers = {
   Query: {
     getCartoons,
     getOneCartoonById,
+  },
+  Mutation: {
+    createCartoon,
   },
 };
 
--- src/resolvers/cartoon.resolver.ts
+++ src/resolvers/cartoon.resolver.ts
@@ -2,10 +2,37 @@ import { default as cartoons } from "../../dataset.json";
 import { Cartoon } from "../types/cartoon.type";
 type GetOneCartoonByIdArgs = {
   id: string;
-}
-export const getCartoons = ():Cartoon[] => {
-  return cartoons;
-} 
-export const getOneCartoonById = (_:unknown, args:GetOneCartoonByIdArgs): Cartoon => {
-  return cartoons.find((cartoon) => cartoon.id === +args.id) as Cartoon;
-}
+};
+let data = cartoons;
+export const getCartoons = (): Cartoon[] => {
+  return data;
+};
+
+export const getOneCartoonById = (
+  _: unknown,
+  args: GetOneCartoonByIdArgs
+): Cartoon => {
+  return data.find((cartoon) => cartoon.id === +args.id) as Cartoon;
+};
+
+export const createCartoon = (
+  _: unknown,
+  args: { cartoon: Cartoon }
+): Number => {
+  const { personnages, ...rest } = args.cartoon;
+  const newPersonnages = personnages.map((pers) => ({
+    ...pers,
+    id: Date.now(),
+  }));
+  const id = +`${data[data.length - 1].id + 1}`;
+  const newCartoon: Cartoon = {
+    ...rest,
+    personnages: newPersonnages,
+    id,
+  };
+
+  data.push(newCartoon);
+  console.log(args);
+  return id;
+};
+
